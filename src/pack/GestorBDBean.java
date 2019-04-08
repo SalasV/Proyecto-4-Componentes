7567233faf30882903286b3e10a428390d6f2c5e
VíctorS <suep2525@gmail.com>
2019-04-08 15:52:15 +0200

    final

--- src/pack/GestorBDBean.java
+++ src/pack/GestorBDBean.java
@@ -8,6 +8,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class GestorBDBean implements Serializable {
@@ -15,7 +16,7 @@ public class GestorBDBean implements Serializable {
 	private String ip;
 	private String bd;
 	private String usuario;
-	private String contraseña;
+	private String contra;
 	private Connection connection;
 	private Statement statement;
 	private ResultSet resultset;
@@ -28,17 +29,17 @@ public class GestorBDBean implements Serializable {
 		addPropertyChangeListener(evt);
 	}
 
-	public void conexion(String ip, String bd, String usuario, String contraseña) {
+	public void conexion(String ip, String bd, String usuario, String contra) {
 		this.ip = ip;
 		this.bd = bd;
 		this.usuario = usuario;
-		this.contraseña = contraseña;
+		this.contra = contra;
 		try {
 			if (connection != null) {
 				desconexion();
 			}
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://" + ip + "/" + bd, usuario, contraseña);
+			connection = DriverManager.getConnection("jdbc:mysql://" + ip + "/" + bd, usuario, contra);
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -65,6 +66,7 @@ public class GestorBDBean implements Serializable {
 			while (resultset.next()) {
 				count++;
 			}
+			System.out.println(count);
 			registro.setBd(this.bd);
 			registro.setUsuario(this.usuario);
 			registro.setTipoConsulta("SELECT");
@@ -137,12 +139,34 @@ public class GestorBDBean implements Serializable {
 		}
 	}
 
-	public void consultar(String db, String dato) {
-		if (dato.equals("SELECT") || dato.equals("DELETE") || dato.equals("UPDATE") || dato.equals("INSERT")) {
+	public void procedure(String consulta) {
+		RegistroBD registro = new RegistroBD();
+
+		Statement st;
+		try {
+			st = (Statement) connection.createStatement();
+			st.execute(consulta);
+			registro.setBd(this.bd);
+			registro.setUsuario(this.usuario);
+			registro.setTipoConsulta("PROCEDURE");
+			registro.setSentencia(consulta);
+			registro.setFechaConsulta(Calendar.getInstance());
+			evento.firePropertyChange("PROCEDURE", null, registro);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.err.println("Error en el procedure");
+		}
+	}
+
+	public ArrayList<RegistroBD> consultar(String db, String dato) {
+		ArrayList<RegistroBD> registros = new ArrayList<>();
+		if (dato.equals("SELECT") || dato.equals("DELETE") || dato.equals("UPDATE") || dato.equals("INSERT")
+				|| dato.equals("PROCEDURE")) {
 			for (RegistroBD registroBD : evt.getLog()) {
 				if (registroBD.getBd().equals(db) && registroBD.getTipoConsulta().equals(dato)) {
 					System.out.println(registroBD.getSentencia() + " - " + registroBD.getFecha() + " - "
 							+ registroBD.getUsuario());
+					registros.add(registroBD);
 				}
 			}
 		} else {
@@ -150,18 +174,23 @@ public class GestorBDBean implements Serializable {
 				if (registroBD.getBd().equals(db) && registroBD.getUsuario().equals(dato)) {
 					System.out.println(registroBD.getSentencia() + " - " + registroBD.getFecha() + " - "
 							+ registroBD.getTipoConsulta());
+					registros.add(registroBD);
 				}
 			}
 		}
+		return registros;
 	}
 
-	public void consultar(String db, String usuario, String tipoConsulta) {
+	public ArrayList<RegistroBD> consultar(String db, String usuario, String tipoConsulta) {
+		ArrayList<RegistroBD> registros = new ArrayList<>();
 		for (RegistroBD registroBD : evt.getLog()) {
 			if (registroBD.getBd().equals(db) && registroBD.getUsuario().equals(usuario)
 					&& registroBD.getTipoConsulta().equals(tipoConsulta)) {
 				System.out.println(registroBD.getSentencia() + " - " + registroBD.getFecha());
+				registros.add(registroBD);
 			}
 		}
+		return registros;
 	}
 
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
